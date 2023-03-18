efc9f7bc10e0fc7a6cc39a7ea6bb9932a4bd2f8d
sks006 <shimkhan07@gmail.com>
2023-03-18 15:22:31 +0600

    third

--- routes/routes.js
+++ routes/routes.js
@@ -1,24 +1,89 @@
+// const authRouter = require("./authRouter")
+// const dashboardRouter = require("./dashboardRouter")
+// const routes = [{
+//         path: "/auth",
+//         handler: authRouter,
+//     },
+//     {
+//         path: "/dashboard",
+//         handler: dashboardRouter,
+//     },
+//     {
+//         path: "/",
+//         handler: (req, res) => {
+//             res.json({
+//                 message: "Hello You",
+//             });
+//         }
+//     }
+// ]
+// module.exports = app => {
+//     routes.forEach(r => {
+//         app.use(r.path, r.handler)
+//     })
+// }
+
+
+
+
 const authRouter = require("./authRouter")
 const dashboardRouter = require("./dashboardRouter")
+
 const routes = [{
-        path: "/auth",
-        handler: authRouter,
+        method: 'get',
+        path: '/auth/signup',
+        handler: authRouter.signupGetController
     },
     {
-        path: "/dashboard",
-        handler: dashboardRouter,
+        method: 'post',
+        path: '/auth/signup',
+        handler: authRouter.signupPostController
     },
     {
-        path: "/",
+        method: 'get',
+        path: '/auth/login',
+        handler: authRouter.loginGetController
+    },
+    {
+        method: 'post',
+        path: '/auth/login',
+        handler: authRouter.loginPostController
+    },
+    {
+        method: 'get',
+        path: '/auth/logout',
+        handler: authRouter.logOutController
+    },
+    {
+        method: 'get',
+        path: '/dashboard',
+        handler: dashboardRouter.dashboardGetController
+    },
+    {
+        method: 'post',
+        path: '/dashboard/search',
+        handler: dashboardRouter.dashboardPostController
+    },
+    {
+        method: 'get',
+        path: '/',
         handler: (req, res) => {
-            res.json({
-                message: "Hello You",
-            });
+            res.json({ message: 'Hello World!' })
         }
     }
 ]
+
 module.exports = app => {
-    routes.forEach(r => {
-        app.use(r.path, r.handler)
-    })
-}
+    routes.forEach(route => {
+        const { method, path, handler } = route;
+        if (method === 'get') {
+            app.get(path, handler);
+        } else if (method === 'post') {
+            app.post(path, handler);
+        } else if (method === 'put') {
+            app.put(path, handler);
+        } else if (method === 'delete') {
+            app.delete(path, handler);
+        }
+    });
+};
